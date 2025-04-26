d1c2997b66c22995ebfc1fce33232d0d18e49122
Wordluc <frangiamoreluca@gmail.com>
2025-04-26 13:19:28 +0200

    refactor: pachage name evaluator

---
+++ eval/env.go
@@ -0,0 +1,41 @@
+package eval
+
+type Environment struct {
+	Variables map[string]Value
+	Function  map[string]Function
+	higherEnv *Environment
+}
+
+func NewEnvironment() *Environment {
+	return &Environment{
+		Variables: make(map[string]Value),
+		Function:  make(map[string]Function),
+	}
+}
+func (e *Environment) AddVariable(name string, v Value) error {
+	e.Variables[name] = v
+	return nil
+}
+
+func (e *Environment) AddFunction(name string, v Function) error {
+	e.Function[name] = v
+	return nil
+}
+
+func (e *Environment) GetVariable(name string) (Value, error) {
+	if v := e.Variables[name]; v != nil {
+		return v, nil
+	}
+	if e.higherEnv == nil {
+		return nil, nil
+	}
+	if v := e.higherEnv.Variables[name]; v != nil {
+		return v, nil
+	}
+	return nil, nil
+}
+
+func (e *Environment) SetHigherEnvironment(env *Environment) error {
+	e.higherEnv = env
+	return nil
+}
---
+++ eval/eval.go
@@ -0,0 +1,66 @@
+package eval
+
+import (
+	"Lugo/parser"
+)
+
+type Program struct {
+	*Environment
+	parser.Lua
+}
+
+func NewEval(tree parser.Lua) *Program {
+	return &Program{
+		NewEnvironment(),
+		tree,
+	}
+}
+
+func (p *Program) Run() error {
+	for _, st := range p.Lua.Statements {
+		switch {
+		case st.StatementVariable != nil:
+			v := st.StatementVariable
+			value, e := p.EvalExp(v.Expression)
+			if e != nil {
+				return e
+			}
+			if e := p.Environment.AddVariable(v.Variable.Name, value); e != nil {
+				return e
+			}
+		case st.StatementFunction != nil:
+			v := st.StatementFunction
+			args := make([]string, len(v.Args))
+			for i := range v.Args {
+				args[i] = v.Args[i].Param
+			}
+			f := Function{
+				v.Body,
+				args,
+				p.Environment,
+			}
+			if e := p.Environment.AddFunction(v.Name, f); e != nil {
+				return e
+			}
+		}
+	}
+	if p.ReturnExpression != nil {
+		v := p.ReturnExpression.ValueReturned
+		value, e := p.EvalExp(*v)
+		if e != nil {
+			return e
+		}
+		p.Environment.AddVariable("return", value)
+		return nil
+	}
+
+	return nil
+}
+
+func (p *Program) EvalExp(exp parser.Expression) (Value, error) {
+	if m := exp.MathExpression; m != nil {
+		return p.EvalMath(m)
+	}
+
+	return nil, nil
+}
